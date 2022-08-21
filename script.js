ebb6841e7635eab95d7132ab783da12547093ed6
carloshnp <carloshnpwork@gmail.com>
2022-08-21 02:52:35 -0300

    funções implementadas: iniciar jogo, jogo (apenas jogadas corretas), fim de jogo, reiniciar jogo

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 let numeroCartas;
 let parrotsCardsShuffled;
+let cartasPareadas = [];
+let cartasSelecionadas = [];
+let cartasClicadas = 0;
+let total = 0;
+
 
 const times = x => f => { if (x > 0) { f()
     times (x - 1) (f) } }
@@ -11,6 +16,8 @@ function inicio() {
     while ((numeroCartas > 14 || numeroCartas < 4) || (numeroCartas%2) !== 0) {
         numeroCartas = parseInt(prompt('Insira um número par de cartas entre 4 e 14!'));
     }
+    let ul = document.querySelector('ul');
+    ul.innerHTML = '' ;
     iniciarJogo();
 }
 
@@ -52,22 +59,47 @@ function iniciarJogo() {
     // Coloca as cartas dos parrots na lista do HTML
     let ul = document.querySelector('ul');
     for (let i=0; i < parrotsCardsShuffled.length; i++) {
-        ul.innerHTML += `<li class='.hidden'>${parrotsCardsShuffled[i]}</li>`;
+        ul.innerHTML += `
+            <li class="teste" onclick="cardClick(this)">
+            <img class='carta hidden' src=${parrotsCardsShuffled[i]}>
+            </li>`;
     }
     // Inicia o jogo
-    jogo();
-}
-
-function jogo() {
-    // Verificar cliques do usuário
-    // Dizer se todas as cartas estão pareadas
-    return '';
 }
 
 function fim() {
-    // Parabéns, você ganhou o jogo em X tempo e Y jogadas!
-    // Deseja jogar novamente?
-    return '';
+    setTimeout(alert('Teste'), 500);
 }
+
+function cardClick(cartaClicada) {
+    if (cartasClicadas < 3) {
+        cartaClicada.firstElementChild.classList.remove('hidden');
+        cartaClicada.classList.add('cartaSelecionada');
+        cartasClicadas++;
+        cartasPareadas.push(cartaClicada.firstElementChild.currentSrc);
+
+        if (cartasClicadas == 2) {
+            
+            if (cartasPareadas[cartasPareadas.length -1] == cartasPareadas[cartasPareadas.length -2]) {
+                let element = document.querySelectorAll('.cartaSelecionada');
+                for (let i=0; i < element.length; i++) {
+                    element[i].removeAttribute('onclick');
+                    element[i].classList.remove('cartaSelecionada');
+                }
+                cartasClicadas = 0;
+                total += 2;
+            }
+            else {
+                cartaClicada.firstElementChild.classList.add('hidden');
+                times (2) (() => cartasPareadas.pop());
+            }
+        }
+    }
+    if (total === numeroCartas) {
+        total = 0;
+        inicio();
+    }
+}
+
 
 inicio();
